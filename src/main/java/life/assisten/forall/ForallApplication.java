e1c8af3bcd45468d0796a04684ef092c87f9bfde
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-14 20:37:38 -0300

    first commit

---
+++ src/main/java/life/assisten/forall/ForallApplication.java
@@ -0,0 +1,13 @@
+package life.assisten.forall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/life/assisten/forall/ForallApplicationTests.java
@@ -0,0 +1,13 @@
+package life.assisten.forall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
